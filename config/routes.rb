79e60da85cbdb48c8d81f2bfc974ee2d5aeede53
opaismoe <wiebe.buursink@gmail.com>
2018-01-03 17:45:17 +0100

    Fix for user/admin show matches

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: "pages#home"
-
-  resources :profiles, only: [:new, :edit, :create, :update]
+  # root to: "pages#home"
   resources :matches
+  resources :profiles, only: [:new, :edit, :create, :update]
 
+  authenticated :user do
+    root 'pages#home', as: :authenticated_root
+  end
+
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
 end
