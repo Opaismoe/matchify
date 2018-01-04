9d191e7bf37ab9284c7ecdf52e95526b85f266b8
opaismoe <wiebe.buursink@gmail.com>
2018-01-04 11:25:12 +0100

    fix install rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of (:email) }
+    it { is_expected.to validate_presence_of (:password) }
+  end
+
+  describe "association with profile" do
+    let(:user) { create :user }
+    let!(:profile) { create :profile, user: user}
+
+    it "has a profile" do
+      user.profile = build(:profile)
+
+      expect(profile.user).to eq(user)
+    end
+
+    it "deletes associated profile" do
+      expect { user.destroy }.to change(Profile, :count).by(-1)
+    end
+  end
 end
