268309d165d97b634395f202c3f08824e1a5e25f
konstantin0s <constantintofan85@gmail.com>
2018-01-04 22:46:32 +0100

    Update matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,5 +3,12 @@ class MatchesController < ApplicationController
   def index
     @matches = Match.all
   end
-  
+
+def create
+  Match.create_matches(params[:date])
+  flash[:notice] = "Matches are Generated!"
+  redirect_to matches_path
+end
+
+
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,9 +4,34 @@ class Match < ApplicationRecord
   validates :group_id, :user_id, presence: true
 
 
-  def self.previous_matches(date)
-    where(date < DateTime.now.beginning_of_day)
-  end
+  #def self.previous_matches(date)
+  #  where(date < DateTime.now.beginning_of_day)
+  #end
+
+  #def self.set_date
+  #  DateTime.now.to_date
+  #end
+
+#def self.match_for_today?
+#  if Match.last == nil
+#    return false
+#  else
+#    date = Match.last.date
+#    return date == set_date
+#  end
+#end
+
+#def self.create_matches
+#  matches = Profile.pairing
+#  matches.each do |match|
+#    group_id = match.fetch(0)
+#    user_id = match.fetch(1)
+#    date = set_date
+#    Match.create(group_id: group_id, user_id: user_id, date: date )
+#  end
+#end
+#end
+
 
   def self.all_students
     @student = User.where(admin:false)
@@ -15,6 +40,26 @@ class Match < ApplicationRecord
       @all_students << student.id
     end
       @all_students
+  end
+
+
+  def self.shuffle_student_ids
+    all_students
+    @shuffled_students_ids = @all_students.shuffle()
+    return @shuffled_students_ids
+  end
+
+
+
+  def self.create_matches(day)
+    shuffle_student_ids
+    id = []
+    while @shuffled_students_ids.length > 1 do
+      2.times{ id << @shuffled_students_ids.slice!(0) }
+      Match.create(group_id: id[0], user_id: id[1], date: day)
+      2.times{ id.delete_at(0) }
+    end
+
   end
 
 
