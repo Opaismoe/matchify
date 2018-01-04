33ad1ed4022365c2a402737dd86f8b0ab630b209
opaismoe <wiebe.buursink@gmail.com>
2018-01-04 12:14:38 +0100

    Delete full name test from Profile model test (moved it to view)

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe Profile, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of (:first_name) }
     it { is_expected.to validate_presence_of (:last_name) }
-    pending "navbar shows full name"
   end
 end
